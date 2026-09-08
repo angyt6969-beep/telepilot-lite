2d1f39af6a83886d7f35157ca76b7a9e988242f7
angyt6969-beep <angyt6969@gmail.com>
2026-09-08 20:59:07 +0300

    Document free-trial bootstrap order

--- free-trial-startup.js
+++ free-trial-startup.js
@@ -1,6 +1,7 @@
 import { Api, Bot } from "grammy";
 import { installFreeTrialTutorialTracking, installFreeTrialUi, installFreeTrialWeb } from "./free-trial.js";
 
+// Free-trial web, tutorial tracking and UI hooks are installed before app startup.
 installFreeTrialWeb();
 installFreeTrialTutorialTracking(Bot);
 installFreeTrialUi(Api);
